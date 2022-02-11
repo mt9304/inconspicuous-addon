99f2f49b92fe213c422d866580705972b057651e
MT9304 <MT9304@gmail.com>
2022-02-10 18:17:05 -0800

    Added links to cups.

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 var hasRunAlready = false;
 
 if (!hasRunAlready) {
-	replaceTextOnDocument("javascript", "HIIIIIIII");
+	replaceTextOnDocument("anime", "CUPS");
 	replacePicsOnDocument();
 	hasRunAlready = true;
 }
@@ -18,13 +18,18 @@ function replaceTextOnDocument(pattern, string, {target = document.body} = {}) {
 
 function replacePicsOnDocument() {
 	var imgs = document.getElementsByTagName("img");
-	var randomImgSrcs = ["https://miro.medium.com/max/1400/1*ZmpgStvNsZXnyUfMGDVj2Q.jpeg", "", "", "", ""];
+	var randomImgSrcs = ["https://www.ikea.com/us/en/images/products/ikea-365-mug-white__0711130_pe727998_s5.jpg?f=s", "https://cb.scene7.com/is/image/Crate/EverydayMugSHS18/$web_pdp_main_carousel_high$/190411135031/everyday-mug.jpg", "https://secure.img1-fg.wfcdn.com/pp/90059160/resize-h600-w600%5Ecompr-r85/1346/13465965/Mugs+%26+Teacups.jpg", "https://cdn.shopify.com/s/files/1/2081/7373/products/452345_1500x.jpg?v=1592238450", "https://cdn.shopify.com/s/files/1/2281/6071/t/161/assets/1766e131ed12--22-UT-MU-COLL.png?v=1642043243"];
 
 	for (var i = 0; i < imgs.length; i++) {
 		var randomNumber = Math.floor((Math.random() * 10)); //Between 0 and 10
 
 		if (randomNumber < 5) {
-			imgs[i].src = newLink;
+			try {
+				imgs[i].src = randomImgSrcs[randomNumber];
+			}
+			catch (e) {
+
+			}
 		}
 	}
 };
